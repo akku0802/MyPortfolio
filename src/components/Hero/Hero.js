ff7536716018b4d2a616ffb4c6726e3eac1ed5d4
Akash <akashtripathi004@gmail.com>
2021-06-29 11:58:48 +0530

    newtab

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -14,7 +14,7 @@ const Hero = () => (
       <SectionText>
       My name is <Span>Akash Tripathi</Span> and i am an enthusiastic fresher, highly motivated, having bachelors of engineering degree in Computer Science and have a good leadership skills. Strong in design and integration with intuitive problem-solving skills. Proficient in C/C++, REACT, and SQL. Passionate about implementing and launching new projects. Looking to start the career as an entry-level software engineer with a reputed firm driven by technology.
       </SectionText>
-      <Button onClick={()=> window.open = 'https://drive.google.com/file/d/1dWWDmXpkkZUw2xfnRhvfgTPFmXwbpAiW/view?usp=sharing'}>Learn More</Button>
+      <Button onClick={()=> window.open('https://drive.google.com/file/d/1dWWDmXpkkZUw2xfnRhvfgTPFmXwbpAiW/view?usp=sharing',"_blank")}>Learn More</Button>
     </LeftSection>
   </Section>
 ); 
